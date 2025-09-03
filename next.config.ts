9f7774b5d434f27eacd916a506a8ec244e74a6fd
Abhishek Mahajan <abhishekmahajan@alphaseam.com>
2025-09-03 13:53:13 +0530

    deploye fix

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // Enable static export instead of next export
+  output: "export",
+
+  // Required because Next.js image optimization doesn’t work with static export
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
